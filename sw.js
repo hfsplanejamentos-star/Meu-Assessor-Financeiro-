b361beed7f280981e5107ac576160ae463848fe2
hfsplanejamentos-star <hfsplanejamentos@gmail.com>
2026-09-19 18:25:44 -0300

    Publish mobile refresh control R10.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='assessor-atual-20260919-r102';
+const C='assessor-atual-20260919-r103';
 const A=['./index.html','./atual.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==C)await caches.delete(k);await self.clients.claim()})()));
