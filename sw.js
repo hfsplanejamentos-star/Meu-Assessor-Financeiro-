824ea7b0a9e0487b5a2046c8e8b87325bafa299f
hfsplanejamentos-star <hfsplanejamentos@gmail.com>
2026-09-19 19:45:20 -0300

    Publish mobile polish R10.6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='assessor-atual-20260919-r105';
+const C='assessor-atual-20260919-r106';
 const A=['./index.html','./atual.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==C)await caches.delete(k);await self.clients.claim()})()));
