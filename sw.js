f4db0237f8999edca703d58d7df645fd97acef91
hfsplanejamentos-star <hfsplanejamentos@gmail.com>
2026-09-20 12:03:56 -0300

    R11.5 refresh current-version PWA cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='assessor-atual-20260920-r114-month-dock';
+const C='assessor-atual-20260920-r115-canonical-mobile';
 const A=['./index.html','./atual.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==C)await caches.delete(k);await self.clients.claim()})()));
