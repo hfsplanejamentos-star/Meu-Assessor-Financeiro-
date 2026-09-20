e8fa81f54e54dde74499abc7b4473d8678d1f571
hfsplanejamentos-star <hfsplanejamentos@gmail.com>
2026-09-20 13:47:12 -0300

    R12.0 refresh cache for real weekly expense chart

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='assessor-atual-20260920-r119-browser-edge';
+const C='assessor-atual-20260920-r120-real-weekly-expenses';
 const A=['./index.html','./atual.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==C)await caches.delete(k);await self.clients.claim()})()));
