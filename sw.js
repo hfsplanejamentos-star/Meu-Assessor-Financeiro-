59fd2f5645afa868aee3166677887dcccc29ce32
hfsplanejamentos-star <hfsplanejamentos@gmail.com>
2026-09-22 11:59:35 -0300

    Invalidar cache apos correcao HTML R13.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='assessor-atual-20260922-r133-canonical';
+const C='assessor-atual-20260922-r134-htmlfix';
 const A=['./index.html','./atual.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==C)await caches.delete(k);await self.clients.claim()})()));
