82127f66f090695161ad4e28f5df0f2205bad455
hfsplanejamentos-star <hfsplanejamentos@gmail.com>
2026-09-23 12:35:23 -0300

    Forcar cache R16.9 clique demonstrativos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='assessor-atual-20260922-r136-specific-statements';
+const C='assessor-atual-20260923-r169-detail-click';
 const A=['./index.html','./atual.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==C)await caches.delete(k);await self.clients.claim()})()));
