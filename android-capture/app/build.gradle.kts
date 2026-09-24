d9a788879bdb6bc501306cd5ecaa407500943074
hfsplanejamentos-star <hfsplanejamentos@gmail.com>
2026-09-24 06:54:15 -0300

    build(android): release version 1.1.3 with custom icon

--- android-capture/app/build.gradle.kts
+++ android-capture/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "br.com.meuassessor.financeiroia"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.2-safe-install"
+        versionCode = 5
+        versionName = "1.1.3-custom-icon"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
