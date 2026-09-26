95061689a1a696ca3b0338cc70e8ddc4e31e4914
hfsplanejamentos-star <hfsplanejamentos@gmail.com>
2026-09-25 22:06:05 -0300

    chore(android): bump capture APK to 1.3.7

--- android-capture/app/build.gradle.kts
+++ android-capture/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "br.com.meuassessor.financeiroia"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.3.6"
+        versionCode = 16
+        versionName = "1.3.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
